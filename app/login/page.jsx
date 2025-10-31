4892ebb66f5b64dab41284942007f92ef9387ecf
Michele Aiko <micheleaikomukaihata@gmail.com>
2025-10-31 09:19:23 -0300

    :lipstick: feat: adicionando hover

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -27,7 +27,7 @@ export default function Login() {
         </div>
       </div>
 
-      <div className = "w-2/5 min-w-100 bg-black rounded-t-[3rem] p-15 mr-25 mt-15"> 
+      <div className = "w-2/5 min-w-100 bg-slate-950 rounded-t-[3rem] p-16 mr-24 mt-10 translate-y-2 transform hover:translate-y-0 transition-all duration-300 ease-linear"> 
       {/* usei rounded-t-[3rem] porque o maior raio padrão da documentação não chega perto da referência */}
         <h2 className="text-center font-league pt-1 text-4xl text-yellow-50 font-bold mb-10 mt-7">BEM VINDO DE VOLTA!</h2>
 
@@ -40,7 +40,7 @@ export default function Login() {
               id = "email" 
               type = "text" 
               placeholder="E-mail" 
-              className="text-lg text-gray-500 font-league w-full pt-1 p-1 pl-4 mb-6 bg-yellow-50 rounded-full">
+              className="text-lg text-gray-500 font-league w-full pt-1 p-1 pl-4 mb-6 bg-yellow-50 rounded-full hover:border-2 hover:border-grey">
             </input>
           </div>
 
@@ -53,7 +53,7 @@ export default function Login() {
               id = "password" 
               type = "text" 
               placeholder="Senha" 
-              className="text-lg text-gray-500 w-full p-1 pl-4 mb-4 bg-yellow-50 rounded-full">
+              className="text-lg text-gray-500 w-full p-1 pl-4 mb-4 bg-yellow-50 rounded-full hover:border-2 hover:border-grey">
             </input>  
           </div>
 
@@ -65,7 +65,7 @@ export default function Login() {
           </div>
 
           <div>
-            <button className="bg-violet-600 text-yellow-50 text-lg w-full rounded-full font-semibold p-1 mb-4">ENTRAR</button>
+            <button className="bg-violet-600 text-yellow-50 text-lg w-full rounded-full font-semibold p-1 mb-4 transform hover:scale-103">ENTRAR</button>
           </div>
 
           <div>
@@ -73,7 +73,7 @@ export default function Login() {
                 Não possui uma conta?  
                 <a 
                   href="INSIRA O LINK" 
-                  className="text-violet-600"
+                  className="text-violet-600 hover:underline"
                 >  Cadastre-se</a>
             </p>
           </div>
