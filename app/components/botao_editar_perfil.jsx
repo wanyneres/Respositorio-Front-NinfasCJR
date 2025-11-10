249984731c60d52d9eeb47236e0fadb606142342
Carol <232050975@aluno.unb.br>
2025-11-10 19:57:55 -0300

    Revert "Merge pull request #3 from wanyneres/feat/modal-editar-perfil"
    This reverts commit 22a0fb3133fae55d97ccec18e52b02f3ebce31cd, reversing
    changes made to 0e0587c1a8d5962fae154f3da1976d80074204c1.

--- app/components/botao_editar_perfil.jsx
+++
@@ -1,5 +0,0 @@
-function BotaoEditarPerfil() {
-    return (<button type="button" className="bg-violet-600 pointer-events-auto text-yellow-50 z-1000 cursor-pointer hover:opacity-90 text-lg w-[324px] h-[43px] rounded-full font-thin">Editar Perfil</button>);
-}
-
-export default BotaoEditarPerfil;
--- app/components/navbar_deslog.jsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-function NavbarDeslog() {
-    return (<Image src="/images/user_deslog.svg" alt="Usuario Deslogado" className="cursor-pointer hover:opacity-90 transition" width={30} height={30}/>);
-}
-
-export default NavbarDeslog;
